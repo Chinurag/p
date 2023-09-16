849c7f9fe0190840351155861a72fbef7382f5ba
Chinurag <129602851+Chinurag@users.noreply.github.com>
2023-09-17 00:40:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const tpbtn2 = document.querySelector(".tpbtn2");
+
+tpbtn2.addEventListener("click", function() {
+    alert("This feature sends a notification to the bus driver, prompting them to stop the BUS!      The button enabling this function becomes active when you are within 1 kilometer of your destination.");
+});
